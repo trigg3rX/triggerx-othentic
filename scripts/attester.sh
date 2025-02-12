f015c5b240d7c78d41b4ce55132dd108d0dd0c58
Nite5ky <aakashayadav@lampros.tech>
2025-02-12 17:40:05 +0530

    fix: connection address for aggregator

--- scripts/attester.sh
+++ scripts/attester.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 othentic-cli node attester \
-    /ip4/127.0.0.1/tcp/9876/p2p/12D3KooWDxDJnLqyvLVNX1PBfnwtTCATuNP5qhm1HX16yVcXF1WY \
+    /ip4/157.173.218.229/tcp/9876/p2p/12D3KooWBNFG1QjuF3UKAKvqhdXcxh9iBmj88cM5eU2EK5Pa91KB \
     --metrics \
     --metrics.port 6061 \
     --avs-webapi http://127.0.0.1 \
